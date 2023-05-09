17d45f60ea64cfc2eac324ce125b0f1bc1a9169f
Patty <patriciamae@1export.com>
2023-05-09 20:30:59 +0800

    Values and Variables

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ if (js === 'amazing') alert('JavaScript is Fun')
 
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10)
+
+console.log("Jonas")
+console.log(23)
+
+let firstName = "Matilda"
+let first = 'jonas'
+let first_name_person 
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let jonas_matilda = "JM"
+let $function = 27;
+let Person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJOb = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher2';
+
+
+console.log(myFirstJob);
