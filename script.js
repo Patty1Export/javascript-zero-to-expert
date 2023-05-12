434c81ff52c78ca0d83ebaf9d5d3810a9689eb4d
Patty <patriciamae@1export.com>
2023-05-12 22:16:02 +0800

    Truthy and Falsy Values

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ if (bmiMarkTest2 > bmiJohnTest2) {
 }
 
 */
-
+/*
 //type conversion
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear);
@@ -181,8 +181,31 @@ console.log(s)
 
 let f = '10' + '4' + '3' - 2 + '5';
 console.log(f)
+*/
 
 
+// 5 falsy value: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+
+const money = 100;
+if(money) {
+    console.log("Don't spend it all");
+} else {
+    console.log("You should get a job!")
+}
+
+
+let height; //falsy
+if(height) {
+    console.log("Yay! height is defined")
+} else {
+    console.log('Height is undefined')
+}
 
 
 
