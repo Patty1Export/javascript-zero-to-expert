4cc5fa393fa1a462cec579d651291f76a7d31fe4
Patty <patriciamae@1export.com>
2023-05-13 22:39:07 +0800

    Functions Calling Other Functions

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ const num = Number('23');
 console.log(num);
 
 */
-
+/*
 //Function Declaration
 // can be call before they are defined in the code
 function calcAge1(birthYear) {
@@ -61,3 +61,49 @@ const calcAge2 = function (birthYeah) {
 const age2 = calcAge2(1992);
 
 console.log(age1, age2);
+*/
+
+
+
+/*
+// Arrow Function
+const calcAge3 = birthYeah => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+ const yearsUntilRequirement = (birthYeah, firstName) => {
+    const age = 2037 - birthYeah;
+    const retirement = 65 - age;
+    // return retirement;
+     return `${firstName}  retires in ${retirement} years`
+ }
+
+ console.log(yearsUntilRequirement(1991 , 'Jonas'))
+ console.log(yearsUntilRequirement(1980 , 'Bob'))
+*/
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges)
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice
+// }
+
+
+
+
+
+
+
+const cutPieces = function (fruit) {
+    return fruit * 4;
+}
+
+const fruitProcessor = function(apples, oranges) {
+    const applePieces = cutPieces(apples)
+    const orangePieces = cutPieces(oranges)
+    const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} pieces of oranges.`;
+    return juice
+}
+
+console.log(fruitProcessor(2, 3));
+
+ 
