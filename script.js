7fc6572386d5c8ba016f49477191f7d09b227d3b
Patty <patriciamae@1export.com>
2023-05-24 20:49:22 +0800

    Basic Array Operations (Methods)

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ checkWinner(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
 */
 
-
+/*
 const friend1 = 'Michael'
 const friend2 = 'Steven'
 const friend3 = 'Peter'
@@ -223,14 +223,45 @@ console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages)
+*/
 
 
+const friends = ['Michael', 'Steven', 'Peter']
 
 
+// add elements
+const newLength = friends.push('jay'); //add the value in the array
+console.log(friends)
+console.log(newLength)
+
+friends.unshift('john');
+console.log(friends);
 
 
+// remove elements
+friends.pop()
+const popped = friends.pop()
+console.log(popped)
+console.log(friends)
+
+friends.shift(); //First
+console.log(friends)
 
 
+console.log(friends.indexOf('Steven'))  //find index
+console.log(friends.indexOf('Bob'))  //find index
+
+
+friends.push(23)
+
+console.log(friends.includes('Steven'))
+console.log(friends.includes('Bob'))
+console.log(friends.includes(23))
+
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven')
+}
 
 
 
